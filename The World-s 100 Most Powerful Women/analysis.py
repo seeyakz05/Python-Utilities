5c5ca53290ee7a600d9c3226b820e0e823572345
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2018-12-07 14:50:51 +0800

    Update analysis.py

--- The World-s 100 Most Powerful Women/analysis.py
+++ The World-s 100 Most Powerful Women/analysis.py
@@ -4,12 +4,8 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from scipy.stats import norm
 
-file_path = r'C:\Users\jjenn\Google Drive\Jie Jenn (YouTube)\Python\100 Powerful Women List Data Analysis' + '\\data.csv'
-raw_data = pd.read_csv(file_path, skiprows=2, encoding='utf-8')
-
-"""
-Let's say I am interested in understand how age plays a factor
-"""
+file_path = 'data.csv'
+raw_data = pd.read_csv(file_path, skiprows=2)
 
 """ Step 1. Data Cleanup """
 # Remove people with no age information available
@@ -20,36 +16,27 @@ data['Age'] = data['Age'].astype(int).values.tolist()
 data.reset_index(inplace=True)
 
 """ Step 2. Perform simple summary statistics """
-# 50% = Median
-# count = Sample population size
 summary = data['Age'].describe()
 
-# here, there are few other things like variance, Skewness, Coefficient of variation I would like to add
+# There are few other things like variance, Skewness, Coefficient of variation I would like to add
 summary['Skewness'] = data['Age'].skew()
 summary['Variance'] = data['Age'].var()
 summary['CV'] = summary['std'] / summary['mean']
 
-
-# here, we can ask questions like.
-# who are the youngest and oldest woman on the Forbe most powerful women in 2018 list
+# Now, we can ask questions like
+# Who are the youngest and oldest woman on the Forbe most powerful women of 2018 list
 print(data[data['Age']==sorted(data['Age'], reverse=True)[0]])
 print(data[data['Age']==sorted(data['Age'], reverse=False)[0]])
 
-# which country accounted for most of the women on the list
+# Which country accounted for most of the women on the list
 print(Counter(data['Country']))
 
-# which industry sector accounted for most of the women on the list
+# Which industry sector accounted for most of the women on the list
 print(Counter(data['Category']))
 
-# graph normal distribution
-# pdf --> Probability density function
+# ---------------------------------
+# Graph normal distribution
+# ---------------------------------
 plt.plot(sorted(data['Age']), norm.pdf(sorted(data['Age']), summary['mean'], summary['std']), '-o')
 plt.hist(data['Age'], normed=True)
-plt.show()
-
-
-# Plot between -10 and 10 with .001 steps.
-x_axis = np.arange(-10, 10, 0.001)
-# Mean = 0, SD = 2.
-plt.plot(x_axis, norm.pdf(x_axis,0,2))
 plt.show()
